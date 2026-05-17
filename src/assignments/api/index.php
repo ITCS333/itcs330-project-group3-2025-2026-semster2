23898b6799c3deebc19c3e65a9a5f02ce43bc216
HessaTalal <202102557@stu.uob.edu.bh>
2026-05-18 02:48:13 +0300

    still working

--- src/assignments/api/index.php
+++ src/assignments/api/index.php
@@ -66,6 +66,10 @@
 // Allow cross-origin requests (CORS) if needed.
 // Allow HTTP methods: GET, POST, PUT, DELETE, OPTIONS.
 // Allow headers: Content-Type, Authorization.
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 
 // TODO: Handle preflight OPTIONS request.
